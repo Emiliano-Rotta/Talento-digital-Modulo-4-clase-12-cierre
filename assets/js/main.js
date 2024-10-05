31ad4b19c7b44373e9c33facf345316d35c9e2f8
Emiliano Rotta <emilianorottaf@gmail.com>
2024-10-04 21:11:11 -0300

    ejercicio terminado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,31 @@
-// "https://rickandmortyapi.com/api/character";
+
+$(document).ready(()=>{
+    const urlApi = "https://rickandmortyapi.com/api/character";
+    const crearCard = ({image, name, status})=>{
+        return `
+        <div class="card">
+            <img src="${image}" alt="">
+            <h2>${name}</h2>
+            <p>Estado:${status === "Alive" ? "Vivo" : "Muerto"}</p>
+        </div> 
+            `
+    }
+
+    $.ajax({
+        url: urlApi,
+        method: "GET",
+        dataType: "json",
+        success: (data)=>{
+            const personajes = data.results
+            let contenidoHTML = ""
+            personajes.forEach(p => {
+                contenidoHTML += crearCard(p)
+            });
+            $("#caracter-container").html(contenidoHTML)
+        },
+        error: (error)=>{
+            console.log(error)
+        }
+    })
+
+})
